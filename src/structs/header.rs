9bd5dd9e0600e12329212f64945aae4283468041
MierenManz <christiaanmens@gmail.com>
2021-10-14 10:28:42 +0200

    add basic header methds

--- src/structs/header.rs
+++ src/structs/header.rs
@@ -21,7 +21,7 @@ pub struct Header {
 }
 
 impl Header {
-    pub fn new(mut r: Reader) -> Result<(), Box<dyn StdError>> {
+    pub fn new(mut r: Reader) -> Result<Header, Box<dyn StdError>> {
         let bytes: Vec<u8> = r.read(7);
         let header_string = String::from_utf8_lossy(&bytes).to_string();
         if header_string != "HL2DEMO" {
@@ -30,20 +30,75 @@ impl Header {
                 format!("Invalid header {}", header_string),
             )));
         }
-        let f: [u8; 4] = match r.read::<Vec<u8>>(4).try_into() {
-            Ok(v) => v,
-            Err(_) => {
-                return Err(Box::new(DemoError::new(
-                    ErrorType::Parsing,
-                    "Could not parse [u8; 4] from bytes",
-                )))
-            }
-        };
-        println!("{:?}", f);
-        // let header = Header {
-        //   header: header_string,
-        //   demo_protocol: u32::from_le_bytes(r.read::<Vec<u8>>(4).try_into())
-        // }
-        Ok(())
+
+        Ok(Header {
+            header: header_string,
+            demo_protocol: u32::from_le_bytes(read_to_le_bytes(&mut r)?),
+            network_protocol: u32::from_le_bytes(read_to_le_bytes(&mut r)?),
+            server_name: String::from_utf8(r.read(260))?,
+            client_name: String::from_utf8(r.read(260))?,
+            map_name: String::from_utf8(r.read(260))?,
+            game_directory: String::from_utf8(r.read(260))?,
+            // Time in seconds
+            playback_time: f32::from_le_bytes(read_to_le_bytes(&mut r)?),
+            ticks: u32::from_le_bytes(read_to_le_bytes(&mut r)?),
+            frames: u32::from_le_bytes(read_to_le_bytes(&mut r)?),
+            sign_on_length: u32::from_le_bytes(read_to_le_bytes(&mut r)?),
+        })
+    }
+
+    pub fn header(self) -> String {
+        self.header
+    }
+
+    pub fn demo_protocol(self) -> u32 {
+        self.demo_protocol
+    }
+
+    pub fn network_protocol(self) -> u32 {
+        self.network_protocol
+    }
+
+    pub fn server_name(self) -> String {
+        self.server_name
+    }
+
+    pub fn client_name(self) -> String {
+        self.client_name
+    }
+
+    pub fn map_name(self) -> String {
+        self.map_name
+    }
+
+    pub fn game_directory(self) -> String {
+        self.game_directory
+    }
+
+    pub fn playback_time(self) -> f32 {
+        self.playback_time
+    }
+
+    pub fn ticks(self) -> u32 {
+        self.ticks
+    }
+
+    pub fn frames(self) -> u32 {
+        self.frames
+    }
+
+    pub fn sign_on_length(self) -> u32 {
+        self.sign_on_length
+    }
+}
+
+#[inline(always)]
+fn read_to_le_bytes(r: &mut Reader) -> Result<[u8; 4], DemoError> {
+    match r.read::<Vec<u8>>(4).try_into() {
+        Ok(v) => Ok(v),
+        Err(_) => Err(DemoError::new(
+            ErrorType::Parsing,
+            "Could not read 4 bytes into array",
+        )),
     }
 }
